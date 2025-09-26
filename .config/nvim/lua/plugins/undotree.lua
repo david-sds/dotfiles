7d64d4120edf49b8aa7ee0d39d94e1d75a8d7d1e
david-sds <dave.sathler@gmail.com>
2025-09-25 22:03:50 -0300

    undotree plugin

---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,10 @@
+-- ================================================================================================
+-- TITLE : undotree
+-- ABOUT : Undotree visualizes the undo history and makes it easy to browse and switch between different undo branches.
+-- LINKS :
+--   > github : https://github.com/mbbill/undotree
+-- ================================================================================================
+
+return {
+	"mbbill/undotree",
+}
