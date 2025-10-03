5eb4b5b63e25db28b6d6f87cfface9cb7f91ae10
david-sds <dave.sathler@gmail.com>
2025-10-02 21:18:03 -0300

    nvim dap wip

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -2,9 +2,11 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"mfussenegger/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
 	},
 	config = function()
-		local dap, dapui = require("dap"), require("dapui")
+		local dap = require("dap")
+		local dapui = require("dapui")
 
 		dapui.setup()
 
@@ -21,33 +23,60 @@ return {
 			dapui.close()
 		end
 
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
 		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>db", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end)
+		vim.keymap.set("n", "<Leader>dr", dap.repl.open)
+		vim.keymap.set("n", "<Leader>dl", dap.run_last)
+		vim.keymap.set("n", "<Leader>du", dapui.toggle)
+		vim.keymap.set("n", "<F5>", dap.continue, {})
+		vim.keymap.set("n", "<F10>", function()
+			require("dap").step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			require("dap").step_into()
+		end)
+		vim.keymap.set("n", "<F12>", function()
+			require("dap").step_out()
+		end)
 
-		vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpoint", {
+			text = "🛑",
+			texthl = "DapBreakpoint",
+			linehl = "DapBreakpoint",
+			numhl = "DapBreakpoint",
+		})
+
+		dap.configurations.dart = {
+			{
+				type = "dart",
+				request = "launch",
+				name = "Launch dart",
+				dartSdkPath = "~/fvm/default/bin/cache/dart-sdk/bin/dart",
+				flutterSdkPath = "~/fvm/default/bin/flutter",
+				program = "${workspaceFolder}/lib/main.dart",
+				cwd = "${workspaceFolder}",
+			},
+			{
+				type = "flutter",
+				request = "launch",
+				name = "Launch flutter",
+				dartSdkPath = "~/fvm/default/bin/cache/dart-sdk/bin/dart", -- ensure this is correct
+				flutterSdkPath = "~/fvm/default/bin/flutter", -- ensure this is correct
+				program = "${workspaceFolder}/lib/main.dart", -- ensure this is correct
+				cwd = "${workspaceFolder}",
+			},
+		}
+		dap.adapters.dart = {
+			type = "executable",
+			command = "dart",
+			args = { "debug_adapter" },
+		}
+		dap.adapters.flutter = {
+			type = "executable",
+			command = "flutter",
+			args = { "debug_adapter" },
+		}
 	end,
 }
-
--- vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
--- vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
--- vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
--- vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
--- vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
--- vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
--- vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
--- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
--- vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
--- vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
---   require('dap.ui.widgets').hover()
--- end)
--- vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
---   require('dap.ui.widgets').preview()
--- end)
--- vim.keymap.set('n', '<Leader>df', function()
---   local widgets = require('dap.ui.widgets')
---   widgets.centered_float(widgets.frames)
--- end)
--- vim.keymap.set('n', '<Leader>ds', function()
---   local widgets = require('dap.ui.widgets')
---   widgets.centered_float(widgets.scopes)
--- end)
