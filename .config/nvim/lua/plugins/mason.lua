10c037a83b4f86e9592b5d0b34f5e68849ef1a09
david-sds <dave.sathler@gmail.com>
2025-10-06 09:58:23 -0300

    mason autoinstall fix

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,17 +3,27 @@
 -- ABOUT : Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters.
 -- LINKS :
 --   > github : https://github.com/mason-org/mason.nvim
+--   > github : https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
 -- ================================================================================================
 
 return {
-	"mason-org/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"lua-language-server",
-			"stylua",
-			"typescript-language-server",
-			"prettierd",
-			"prisma-language-server",
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"typescript-language-server",
+				"prisma-language-server",
+				"prettierd",
+				"stylua",
+			},
+			auto_update = false,
+			run_on_start = true,
 		},
 	},
 }
