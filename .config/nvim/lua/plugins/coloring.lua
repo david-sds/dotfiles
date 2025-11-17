deedf49442428b285da1e9ffa0d9faed42c14be4
david-sds <dave.sathler@gmail.com>
2025-11-17 16:09:20 -0300

    color picker shortcut change

--- .config/nvim/lua/plugins/coloring.lua
+++ .config/nvim/lua/plugins/coloring.lua
@@ -13,7 +13,7 @@ return {
 		version = "*",
 		keys = {
 			{
-				"<leader>c",
+				"<leader>cp",
 				function()
 					require("oklch-color-picker").pick_under_cursor({ fallback_open = {} })
 				end,
