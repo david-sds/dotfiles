306e1f02bae3957fa578512095c02390f3bfc2b4
david-sds <dave.sathler@gmail.com>
2025-12-11 19:56:54 -0300

    binbows startup mount gdrive

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 xbindkeys
 xfconf-query -c xfwm4 -p /general/workspace_count -s 4
 paplay /home/david/.local/share/sounds/Smooth/stereo/winxp_startup.mp3
+rclone mount gdrive: ~/GDrive --dir-cache-time 72h --vfs-cache-mode writes --allow-other &
