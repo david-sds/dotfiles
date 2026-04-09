50a8f61da7765a04f5bfe48977470c5917207149
david-sds <dave.sathler@gmail.com>
2026-04-09 13:36:05 -0300

    folder reoeganization

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s "$(pwd)/alacritty" "$HOME/.config/alacritty"
+ln -s "$(pwd)/codex" "$HOME/.config/codex"
+ln -s "$(pwd)/nvim" "$HOME/.config/nvim"
+ln -s "$(pwd)/opencode" "$HOME/.config/opencode"
+ln -s "$(pwd)/tmux" "$HOME/.config/tmux"
