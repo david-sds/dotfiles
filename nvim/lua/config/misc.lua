7546bafd535ad4dafd6ce2f19aeff8631707b38e
david-sds <dave.sathler@gmail.com>
2026-04-16 19:41:41 -0300

    bases have yaml syntax

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -1,3 +1,8 @@
+-- ============================================================================
+-- TITLE : miscellaneous
+-- ABOUT : small configs that don't fit the other major categories
+-- ============================================================================
+
 vim.filetype.add({
 	extension = {
 		base = "yaml",
