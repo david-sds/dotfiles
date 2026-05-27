f6ea132e82aa48d02235b8eb3f5371efa036c5d9
david-sds <dave.sathler@gmail.com>
2026-05-27 13:03:16 -0300

    nvim plugins organization

--- nvim/lua/plugins/grammar.lua
+++ nvim/lua/plugins/grammar.lua
@@ -105,39 +105,3 @@ require("mason-tool-installer").setup({
 require("mason").setup()
 
 vim.keymap.set("n", "<leader>M", "<CMD>Mason<CR>", { desc = "Open Mason menu" })
-
--- ============================================================================
--- TITLE : trouble.nvim
--- ABOUT : A pretty diagnostics, references, quickfix and location list viewer for Neovim.
--- ============================================================================
-vim.pack.add({ "https://github.com/folke/trouble.nvim" })
-
-vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Workspace Diagnostics (Trouble)" })
-vim.keymap.set(
-	"n",
-	"<leader>xX",
-	"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-	{ desc = "Buffer Diagnostics (Trouble)" }
-)
-vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>", { desc = "Symbols (Trouble)" })
-vim.keymap.set(
-	"n",
-	"<leader>cl",
-	"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-	{ desc = "LSP Definitions / references / ... (Trouble)" }
-)
-vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<CR>", { desc = "Location List (Trouble)" })
-vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<CR>", { desc = "Quickfix List (Trouble)" })
-
----@type trouble.Config
-require("trouble").setup({})
-
--- Darkens background color of trouble.nvim buffer (gruvbox-material)
-local trouble_colors_group = vim.api.nvim_create_augroup("TroubleColorsGroup", { clear = true })
-vim.api.nvim_create_autocmd("ColorScheme", {
-	group = trouble_colors_group,
-	callback = function()
-		vim.api.nvim_set_hl(0, "TroubleNormal", { link = "NvimTreeNormal" })
-		vim.api.nvim_set_hl(0, "TroubleNormalNC", { link = "NvimTreeNormalNC" })
-	end,
-})
