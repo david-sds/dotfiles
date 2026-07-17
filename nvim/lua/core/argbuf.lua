d802bef13176dc1597938834fbd12cf6d2139ffb
david-sds <dave.sathler@gmail.com>
2026-07-17 13:26:29 -0300

    binbows push-conf-master at 2026-07-17 13:26:29

---
+++ nvim/lua/core/argbuf.lua
@@ -0,0 +1,143 @@
+-- Manage Argument List
+local argsdir = "~/.local/share/nvim/argsdir" -- args directory path
+local argsdir_path = vim.fn.expand(argsdir)
+if vim.fn.isdirectory(argsdir_path) == 0 then
+	vim.fn.mkdir(argsdir_path, "p") -- Create if not exists
+end
+local argbuf_name = "argbuf://"
+
+local function get_current_path()
+	return argsdir_path .. "/" .. vim.fn.getcwd():gsub("/", "_")
+end
+
+local function get_args_state()
+	local args = vim.fn.argv()
+	if type(args) == "string" then
+		args = { args }
+	end
+	return args
+end
+
+local function add_arg_state(file)
+	local args = get_args_state()
+	for _, arg in ipairs(args) do
+		if vim.fn.fnamemodify(arg, ":p") == file then
+			return
+		end
+	end
+	vim.cmd("argadd " .. file)
+end
+
+local function persist()
+	local current_path = get_current_path()
+	local args = get_args_state()
+	vim.fn.writefile(args, current_path)
+end
+
+local function reset_state()
+	vim.cmd("%argdelete")
+end
+
+local function load()
+	reset_state()
+	local current_path = get_current_path()
+	if vim.fn.filereadable(current_path) == 0 then
+		return
+	end
+	local lines = vim.fn.readfile(current_path)
+	for _, arg in ipairs(lines) do
+		add_arg_state(arg)
+	end
+	persist()
+end
+
+local function add_current_file()
+	add_arg_state(vim.fn.expand("%:p"))
+	persist()
+end
+
+local function get_arg_buf()
+	local buf = vim.fn.bufnr(argbuf_name)
+	if buf == -1 then
+		buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_name(buf, argbuf_name)
+	end
+
+	vim.keymap.set("n", "<CR>", function()
+		local line = vim.api.nvim_get_current_line():gsub("%s+$", "")
+		if line == "" then
+			return
+		end
+		vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
+		vim.cmd("e " .. line)
+	end, { buffer = buf, nowait = true, silent = true })
+	vim.keymap.set("n", "q", "<CMD>quit<CR>", { buffer = buf, nowait = true, silent = true })
+
+	vim.bo[buf].buftype = "acwrite"
+	vim.bo[buf].bufhidden = "hide"
+
+	return buf
+end
+
+local function open_arg_buf_win()
+	local buf = get_arg_buf()
+	local args_state = get_args_state()
+	local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+	if not vim.deep_equal(current_lines, args_state) then
+		vim.api.nvim_buf_set_lines(buf, 0, -1, true, args_state)
+	end
+
+	local width = math.floor(vim.o.columns * 0.40)
+	local height = math.floor(vim.o.lines * 0.40)
+	return vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = math.floor((vim.o.columns - width) / 2),
+		row = math.floor((vim.o.lines - height) / 2),
+		border = "rounded",
+	})
+end
+
+-- Autocommands
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+	callback = load,
+})
+
+vim.api.nvim_create_autocmd("BufWriteCmd", {
+	pattern = argbuf_name,
+	callback = function()
+		local buf = vim.fn.bufnr(argbuf_name)
+		if buf == -1 then
+			return
+		end
+
+		local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+		reset_state()
+		for _, line in ipairs(lines) do
+			local l = line:gsub("%s+$", "")
+			if l ~= "" then
+				add_arg_state(l)
+			end
+		end
+		persist()
+		load()
+
+		vim.bo[buf].modified = false
+		vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
+	end,
+})
+
+-- Harpoon bindings
+vim.keymap.set("n", "<leader>h", add_current_file, { desc = "Add to Argument list" })
+vim.keymap.set("n", "<leader>H", open_arg_buf_win, { desc = "Open argument edit buffer" })
+for i = 1, 10 do
+	local key = tostring(i % 10)
+	vim.keymap.set(
+		"n",
+		"<leader>" .. key,
+		"<CMD>argu " .. i .. "<CR>",
+		{ desc = string.format("Go to argument %d", i) }
+	)
+end
