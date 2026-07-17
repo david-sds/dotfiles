8bd24ffbcb4ce9c6894cefd279b60c9446f10e28
david-sds <dave.sathler@gmail.com>
2026-07-17 14:36:28 -0300

    fagundez push-conf-master at 2026-07-17 14:36:28

--- nvim/lua/core/argbuf.lua
+++ nvim/lua/core/argbuf.lua
@@ -10,7 +10,7 @@ local function get_current_path()
 	return argsdir_path .. "/" .. vim.fn.getcwd():gsub("/", "_")
 end
 
-local function get_args_state()
+local function get_args()
 	local args = vim.fn.argv()
 	if type(args) == "string" then
 		args = { args }
@@ -18,8 +18,8 @@ local function get_args_state()
 	return args
 end
 
-local function add_arg_state(file)
-	local args = get_args_state()
+local function add_arg(file)
+	local args = get_args()
 	for _, arg in ipairs(args) do
 		if vim.fn.fnamemodify(arg, ":p") == file then
 			return
@@ -30,7 +30,7 @@ end
 
 local function persist()
 	local current_path = get_current_path()
-	local args = get_args_state()
+	local args = get_args()
 	vim.fn.writefile(args, current_path)
 end
 
@@ -46,13 +46,13 @@ local function load()
 	end
 	local lines = vim.fn.readfile(current_path)
 	for _, arg in ipairs(lines) do
-		add_arg_state(arg)
+		add_arg(arg)
 	end
 	persist()
 end
 
 local function add_current_file()
-	add_arg_state(vim.fn.expand("%:p"))
+	add_arg(vim.fn.expand("%:p"))
 	persist()
 end
 
@@ -71,7 +71,19 @@ local function get_arg_buf()
 		vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
 		vim.cmd("e " .. line)
 	end, { buffer = buf, nowait = true, silent = true })
-	vim.keymap.set("n", "q", "<CMD>quit<CR>", { buffer = buf, nowait = true, silent = true })
+
+	vim.keymap.set("n", "q", function()
+		vim.bo[buf].modified = false
+		vim.cmd("quit")
+	end, { buffer = buf, nowait = true, silent = true })
+
+	vim.api.nvim_create_autocmd("WinClosed", {
+		buffer = buf,
+		once = true,
+		callback = function()
+			vim.api.nvim_buf_delete(buf, { force = true })
+		end,
+	})
 
 	vim.bo[buf].buftype = "acwrite"
 	vim.bo[buf].bufhidden = "hide"
@@ -81,7 +93,7 @@ end
 
 local function open_arg_buf_win()
 	local buf = get_arg_buf()
-	local args_state = get_args_state()
+	local args_state = get_args()
 	local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 	if not vim.deep_equal(current_lines, args_state) then
 		vim.api.nvim_buf_set_lines(buf, 0, -1, true, args_state)
@@ -118,7 +130,7 @@ vim.api.nvim_create_autocmd("BufWriteCmd", {
 		for _, line in ipairs(lines) do
 			local l = line:gsub("%s+$", "")
 			if l ~= "" then
-				add_arg_state(l)
+				add_arg(l)
 			end
 		end
 		persist()
